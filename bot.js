282ef092f238985e9d78d30037134af59ca6dfdb
GK7O <45012199+GK7O@users.noreply.github.com>
2019-01-14 22:00:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-var prefix = "1";
-var adminprefix = 's'
+var prefix = "5";
+var adminprefix = 'h'
 /////////////////////////
 ////////////////////////
 
@@ -314,10 +314,10 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-    if (message.content === '1help') {
+    if (message.content === '5help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (,1,)**')
+        .setDescription('**برفكس البوت (,5,)**')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('disconnect', 'الخروج من رومك الصوتي')
